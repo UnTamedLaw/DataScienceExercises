09956c3ccf549790c4ca8764dfe6f545b4adddb3
Kevin Law <kevin@Thaumaturge-Pro.local>
2020-01-17 16:19:38 -0800

    finished week1 prep

--- DataScienceJobMock.py
+++ DataScienceJobMock.py
@@ -1,4 +1,7 @@
-#dict contains the user's id and a name
+from collections import Counter
+from collections import defaultdict
+
+#lists of users that is represented by a dictionary contains the user's id and a name
 users = [
     { "id": 0, "name": "Hero" },
     { "id": 1, "name": "Dunn" },
@@ -19,9 +22,9 @@ friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
 friendships = {user["id"]: [] for user in users}
 
 # and loop over the friendship pairs to populate it:
-for i, j in friendship_pairs:
-    friendships[i].append(j) # add j as a friend of user i
-    friendships[j].append(i) # add i ad a friend of user j
+for friend1, friend2 in friendship_pairs:
+    friendships[friend2].append(friend1) # add friend1 as a friend of friend2
+    friendships[friend1].append(friend2) # add friend2 as a friend of friend1
 
 def number_of_friends(user):
     """How many friends does _user_ have?"""
@@ -41,8 +44,116 @@ num_friends_by_id = [(user["id"], number_of_friends(user))
                      for user in users]
 
 #lambda arguments : expression
-
+#difference between sort and sorted is that sort will modify the list that it is called in
+#sorted creates a new list 
 num_friends_by_id.sort(                                             # sort the list
     key = lambda id_and_friends: id_and_friends[1],                 # by num_friends
     reverse = True)                                                 # largest to smallest
+#find friend of a friend
+def find_foaf(user):
+    return [foaf_id
+            for friend_id in friendships[user["id"]] 
+            for foaf_id in friendships[friend_id]]
+#print(find_foaf(users[0]))
 
+#count of mutual friends that excludes people already known to the user
+def count_of_friends_of_friends(user):
+    user_id = user["id"]
+    return Counter(
+        foaf_id
+        for friend_id in friendships[user_id] #for each of my friends
+        for foaf_id in friendships[friend_id] #find their friends
+        if foaf_id != user_id                 # who aren't me
+        and foaf_id not in friendships[user_id] #and aren't my friends
+    )
+#print(count_of_friends_of_friends(users[3])) 
+#Counter ({0: 2, 5:1}) means that user id 3, Chi has two mutual friends with Hero(id0) but only one mutual friend with Clive(id5).
+
+#lists of pairs with user_id and their interests
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+#function that find users with certain interests
+
+def data_scientists_who_like(target_interest):
+    return [user_id
+            for user_id, user_interest in interests
+            if user_interest == target_interest]
+
+#keys are interests, values are list of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+#keys are user_ids, values are lists of interests for that user_id
+interests_by_user_id = defaultdict(list)
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+#who has the most interests in common with a given user
+def most_common_interests_with(user):
+    return Counter(
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
+    )
+
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+                        (48000, 0.7), (76000, 6),
+                        (69000, 6.5), (76000, 7.5),
+                        (60000, 2.5), (83000, 10),
+                        (48000, 1.9), (63000, 4.2)]
+
+#keys are years, values are lists of the salaries for each tenure
+salary_by_tenure = defaultdict(list)
+for salary, tenure in salaries_and_tenures:
+    salary_by_tenure[tenure].append(salary)
+
+#keys are years, each value is average salary for that tenure
+average_salary_by_tenure = {
+    tenure: sum(salaries) / len(salaries)
+    for tenure, salaries in salary_by_tenure.items()
+}
+#group tenures together
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than two"
+    elif tenure < 5:
+        return "between two and five"
+    else:
+        return "more than five"
+
+#keys are tenure buckets, values are list of salaries for that bucket
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    bucket = tenure_bucket(tenure)
+    salary_by_tenure_bucket[bucket].append(salary)
+
+#keys are tenure buckets, values are average salary for that bucket
+average_salary_by_bucket = {
+    tenure_bucket: sum(salaries) /len(salaries)
+    for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+}
+#find popular interests
+words_and_counts = Counter(word
+                            for user, interest in interests
+                            for word in interest.lower().split())
+for word, count in words_and_counts.most_common():
+    if count >1:
+       a print(word, count)
